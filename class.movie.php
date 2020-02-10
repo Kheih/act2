2c5ed0bfe2ea5d8256e04e4a7cf950a88c169da8
Kheih <56195049+Kheih@users.noreply.github.com>
2020-02-10 11:05:14 -0400

    Create class.movie.php

---
+++ class.movie.php
@@ -0,0 +1,25 @@
+
+<?php
+class movie{
+	public $database;
+	
+	public function __construct(){
+		$this->database = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		if(mysqli_connect_errno()){
+			echo "Database connection error.";
+			exit;
+		}else {
+		}
+	}
+	
+	public function get_movie(){
+		$sql = "SELECT * FROM tblmovie";
+		$result = mysqli_query($this->database,$sql);
+		while($row = mysqli_fetch_assoc($result)){
+			$list[] = $row;
+		}
+		return $list;
+	}
+	
+}
+?>
